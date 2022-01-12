13b784b2975b4ed29f26a69efbbac95a5e5792c8
MarkLewis <techliujing@gmail.com>
2022-01-12 17:55:23 +0800

    修改为CU的样式

--- ToDoList/BiDAppDelegate.m
+++ ToDoList/BiDAppDelegate.m
@@ -7,47 +7,36 @@
 //
 
 #import "BiDAppDelegate.h"
-#import "BiDToDoListViewController.h"
+#import "CU_TabBarController.h"
+#import "CU_ChangeLanguageTool.h"
+#import "GKNavigationBarConfigure.h"
 @implementation BiDAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  UIViewController *controller = [[UIViewController alloc] init];
-  controller.title = @"home";
-  controller.view.backgroundColor = [UIColor grayColor];
-  self.window.rootViewController = controller;
+  self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+  [self.window makeKeyWindow];
+  [self.window makeKeyAndVisible];
+  [self configNav] ;//导航栏配置
+  //初始化应用语言
+  [CU_ChangeLanguageTool initUserLanguage];
+  
+  CU_TabBarController *vc = [[CU_TabBarController alloc] init] ;
+  self.window.rootViewController = vc ;
   return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    
-    // self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];    
-    // [self.window makeKeyAndVisible];
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+#pragma mark - 设置导航栏
+-(void) configNav{
+    // 统一设置导航栏样式
+    GKNavigationBarConfigure *configure = [GKNavigationBarConfigure sharedInstance];
+    [configure setupDefaultConfigure];
+    // 设置自定义样式
+    configure.backgroundColor = [UIColor whiteColor];
+    configure.titleColor = [UIColor blackColor];
+    configure.titleFont  = kBoldFont(16);
+    configure.backStyle = GKNavigationBarBackStyleBlack ;
+    configure.gk_navItemLeftSpace = 8 ;
+    configure.gk_navItemRightSpace = 8 ;
 }
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
-
 @end
