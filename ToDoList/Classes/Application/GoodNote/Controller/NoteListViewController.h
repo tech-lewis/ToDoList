bad7ac24799c9b8dfe0c0354e9c45383364c4103
Mark <marklewis@qq.com>
2022-01-26 22:50:25 +0800

    添加数据库相关文件

---
+++ ToDoList/Classes/Application/GoodNote/Controller/NoteListViewController.h
@@ -0,0 +1,17 @@
+//
+//  NoteListViewController.h
+//  ToDoList
+//
+//  Created by MarkLewis on 2022/1/26.
+//  Copyright © 2022 TechLewis. All rights reserved.
+//
+
+#import "CU_BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NoteListViewController : CU_BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ToDoList/Classes/Application/GoodNote/Controller/NoteListViewController.m
@@ -0,0 +1,32 @@
+//
+//  NoteListViewController.m
+//  ToDoList
+//
+//  Created by MarkLewis on 2022/1/26.
+//  Copyright © 2022 TechLewis. All rights reserved.
+//
+
+#import "NoteListViewController.h"
+
+@interface NoteListViewController ()
+
+@end
+
+@implementation NoteListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
