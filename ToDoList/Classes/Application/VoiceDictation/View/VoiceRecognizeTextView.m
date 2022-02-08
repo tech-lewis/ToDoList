6621ed9df0d3e048351a2128edaba8a7dd891176
MarkLewis <techliujing@gmail.com>
2022-02-08 10:33:44 +0800

    自定义长按Text View的弹出菜单

--- ToDoList/Classes/Application/VoiceDictation/View/VoiceRecognizeTextView.m
+++ ToDoList/Classes/Application/VoiceDictation/View/VoiceRecognizeTextView.m
@@ -10,12 +10,37 @@
 
 @implementation VoiceRecognizeTextView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (BOOL)canPerformAction:(SEL)action withSender:(id)sender
+{
+  UIMenuItem * item1=[[UIMenuItem alloc]initWithTitle:@"复制" action:@selector(copy:)];
+  UIMenuItem * item2=[[UIMenuItem alloc]initWithTitle:@"文本对比" action:@selector(select:)];
+  UIMenuItem * item3=[[UIMenuItem alloc]initWithTitle:@"音标/拼音" action:@selector(read:)];
+  //创建UIMenuController
+  UIMenuController * menuController=[UIMenuController sharedMenuController];
+  menuController.menuItems= @[item1, item2, item3];
+ 
+  BOOL result = (action == @selector(copy:) || action == @selector(select:) || action == @selector(read:) || action == @selector(copy:) || action == @selector(copy:) || action == @selector(copy:));
+  
+  return  result;
 }
-*/
 
+/*系统提供的有一些公用的方法,只需要实现出来,对应的menuItem就会加上去 */
+-(void)copy:(id)sender
+{
+    NSLog(@"复制");
+}
+-(void)select:(id)sender
+{
+    NSLog(@"粘贴");
+}
+-(void)read:(id)sender
+{}
+
+-(void)selectAll:(id)sender
+{}
+
+-(void)restet:(UIMenuItem *)item
+{
+    NSLog(@"剪切");
+}
 @end
