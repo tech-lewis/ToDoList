7ec80784af6068f4d9b5f0741af9dde14caed6ac
MarkLewis <techliujing@gmail.com>
2022-02-08 10:12:04 +0800

    添加自定义到的textview

---
+++ ToDoList/Classes/Application/VoiceDictation/View/VoiceRecognizeTextView.h
@@ -0,0 +1,17 @@
+//
+//  VoiceRecognizeTextView.h
+//  ToDoList
+//
+//  Created by MarkLewis on 2022/2/7.
+//  Copyright © 2022 TechLewis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface VoiceRecognizeTextView : UITextView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ToDoList/Classes/Application/VoiceDictation/View/VoiceRecognizeTextView.m
@@ -0,0 +1,21 @@
+//
+//  VoiceRecognizeTextView.m
+//  ToDoList
+//
+//  Created by MarkLewis on 2022/2/7.
+//  Copyright © 2022 TechLewis. All rights reserved.
+//
+
+#import "VoiceRecognizeTextView.h"
+
+@implementation VoiceRecognizeTextView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
