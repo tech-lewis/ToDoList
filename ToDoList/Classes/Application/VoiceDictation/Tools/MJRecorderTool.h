82917a32dc88f8dadcc0dfee11a8bed5d6f0bb71
MarkLewis <techliujing@gmail.com>
2022-03-31 15:29:35 +0800

    fix: 调整录音的代码

---
+++ ToDoList/Classes/Application/VoiceDictation/Tools/MJRecorderTool.h
@@ -0,0 +1,17 @@
+//
+//  MJRecorderTool.h
+//  ToDoList
+//
+//  Created by MarkLwis on 2022/2/9.
+//  Copyright © 2022 TechLewis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MJRecorderTool : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ToDoList/Classes/Application/VoiceDictation/Tools/MJRecorderTool.m
@@ -0,0 +1,13 @@
+//
+//  MJRecorderTool.m
+//  ToDoList
+//
+//  Created by MarkLwis on 2022/2/9.
+//  Copyright © 2022 TechLewis. All rights reserved.
+//
+
+#import "MJRecorderTool.h"
+
+@implementation MJRecorderTool
+
+@end
